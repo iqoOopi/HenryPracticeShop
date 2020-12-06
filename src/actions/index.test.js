2b30191815fd216a97222379a7d7c764bdeeb8fd
Henry <haotian.zhang1989@hotmail.com>
2020-12-06 16:04:21 -0700

    looking into automate testing

---
+++ src/actions/index.test.js
@@ -0,0 +1,5 @@
+import * as actions from './index'
+import * as types from './types'
+
+//https://redux.js.org/recipes/writing-tests
+// Async Action Creator as I use thunk
--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import SearchBar from './SearchBar';
 
-//this won't work for now as I will need to figure out how to pass store in 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<SearchBar />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
