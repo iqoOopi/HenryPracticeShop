e7ec4d9b0d5ff5f5e98b5067410da9ae15a08653
Henry <haotian.zhang1989@hotmail.com>
2020-12-06 17:17:13 -0700

    Update SearchBar.test.js

--- src/components/SearchBar.test.js
+++ src/components/SearchBar.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './SearchBar';
 
+//this won't work for now as I will need to figure out how to pass store in 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<SearchBar />, div);
